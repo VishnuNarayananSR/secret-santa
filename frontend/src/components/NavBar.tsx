639cb59724cbeba443052ca49a6ce4162dba0d62
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2024-12-24 13:03:20 +0530

    feat(ui: navbar, logo): complete

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import { Navbar, NavbarBrand, Image } from "@nextui-org/react";
+import logo from "../assets/logo.svg";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Navbar className="bg-primary-600 shadow-medium">
+      <NavbarBrand>
+        <Link
+          to="/"
+          className="text-foreground-50 font-bold text-xl flex items-center"
+        >
+          <Image src={logo} alt="Logo" className="size-14 mr-2" />
+          <p className="font-bold text-inherit">Secret Sleigh</p>
+        </Link>
+      </NavbarBrand>
+    </Navbar>
+  );
 };
 
 export default NavBar;
