e4ef1af0aca5b46f284f896874322a0bb9d67cdf
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2024-12-25 23:52:58 +0530

    feat(ui: serve): integrate react build into server side

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,19 @@ import connectDB from "./config/db";
 import groupRoutes from "./routes/groupRoutes";
 import participantRoutes from "./routes/participantRoutes";
 import { PORT } from "./constants";
+import path from "path";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 app.use("/api/groups", groupRoutes);
 app.use("/api/participants", participantRoutes);
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+});
 
 connectDB();
 app.listen(PORT, () =>
